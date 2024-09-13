834b927c16fe6648af2ef1efbf106e362d0a79f3
Giovanni Esteban Moreno Urbina <ingestebanmu@hotmail.com>
2024-09-13 12:24:10 -0500

    Inicializar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,13 +16,14 @@ function Home() {
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
+                  👋🏽
                 </span>
               </h1>
 
-              <h1 className="heading-name">
+              <h1 className="heading-name" aria-label="Giovanni Esteban Moreno Urbina">
                 I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                <strong className="main-name"> Gio</strong>
+
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
